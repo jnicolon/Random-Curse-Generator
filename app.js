4df9867893d376b2cc75decf3e31ef649c15fcb8
jnicolon <63628726+jnicolon@users.noreply.github.com>
2020-06-22 23:23:50 -0400

    four flames

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+const flame = document.querySelector('#flame');
+
+
+// let tl = gsap.timeline();
+
+
+
+// document.querySelector('#start').addEventListener('click', ()=> {
+//     tl.fromTo('#flame', {y:0, x:250, opacity: 0}, {y:0, x:250, opacity: 1, duration: 2}, )
+//     .to("#flame", {y:450, x:120, ease:"Power1.easeIn", duration: 3})
+//     .to('#flame', {y:172, x:488, ease:"Power1.easeIn", duration: 2})
+//     .to('#flame', {y:172, x:2, ease:"Power1.easeIn", duration: 2})
+//     .to('#flame', {y:450, x:398, ease:"Power1.easeIn", duration: 2})
+//     .to('#flame', {y:0, x:250, ease:"Power1.easeIn", duration: 2})
+        
+
+        
+//     }
+// )
